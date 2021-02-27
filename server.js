6563c0642608652c2a00543c9984c32e4c4da0db
impez <hicherkmic11@gmail.com>
2021-02-27 22:45:17 +0100

    First init

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "variables.env" });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log("Server listening");
+});
